0fe3adb14133114ebff48ced2fdd957b84d1c7f8
Kangnvd <ngocthiennguyen28052004@gmail.com>
2023-05-30 21:58:56 +0700

    chuyen slide

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-window.addEventListener("load", function(){
-const sliderPrev = document.querySelector(".slider-prev");
-const sliderNext = document.querySelector(".slider-next");
-const slider = document.querySelector(".slider");
-const sliderMain = document.querySelector(".slider-main");
-const sliderItems = document.querySelectorAll(".slider-item");
-const sliderDots = document.querySelectorAll(".slider-dot-item");
-const sliderItemWidth = sliderItems[0].offsetWidth;
-const sliderLength = sliderItems.length; 
-let positionX = 0;
-let index = 0;
-sliderNext.addEventListener("click", function () {
-    moveSlider(1);
-});
-sliderPrev.addEventListener("click", function() {
-    moveSlider(-1);
-});
-function moveSlider (direction){
-    if (direction === 1){
-        index++
-        if (index >= sliderLength) return;
-        positionX = positionX - sliderItemWidth;
-        sliderMain.style = 'transform: translateX{$(positionX)px}';
+window.addEventListener("load", function () {
+    const sliderPrev = document.querySelector(".slider-prev");
+    const sliderNext = document.querySelector(".slider-next");
+    const slider = document.querySelector(".slider");
+    const sliderMain = document.querySelector(".slider-main");
+    const sliderItems = document.querySelectorAll(".slider-item");
+    const sliderDots = document.querySelectorAll(".slider-dot-item");
+    const sliderItemWidth = sliderItems[0].offsetWidth;
+    const sliderLength = sliderItems.length;
+    let positionX = 0;
+    let index = 0;
+    sliderNext.addEventListener("click", function () {
+        moveSlider(1);
+    });
+    sliderPrev.addEventListener("click", function () {
+        moveSlider(-1);
+    });
+    [...sliderDots].forEach((item) =>
+        item.addEventListener("click", function (e) {
+            [...sliderDots].forEach((el) => el.classList.remove("active"));
+            e.target.classList.add("active");
+            const slideIndex = parseInt(e.target.dataset.index);
+            index = slideIndex;
+            positionX = -1 * index * sliderItemWidth;
+            sliderMain.style = 'transform: translateX(' + positionX + 'px)';
+        })
+    )
+    function moveSlider(direction) {
+        if (direction === 1) {
 
-    } else if (direction === -1){
+            if (index >= sliderLength - 1) {
+                index = sliderLength - 1;
+                return;
+            };
 
+            positionX = positionX - sliderItemWidth;
+            sliderMain.style = 'transform: translateX(' + positionX + 'px)';
+            index++;
+        } else if (direction === -1) {
+
+            if (index <= 0) {
+                index = 0;
+                return;
+            };
+            positionX = positionX + sliderItemWidth;
+            sliderMain.style = 'transform: translateX(' + positionX + 'px)';
+            index--;
+        };
+        [...sliderDots].forEach((el) => el.classList.remove("active"));
+        sliderDots[index].classList.add("active");
     }
-}
 });
