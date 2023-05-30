feefe1864826e4c7c80afbc99d18aefadc998f76
Kangnvd <ngocthiennguyen28052004@gmail.com>
2023-05-30 21:59:08 +0700

    chuyen slide

--- index.js
+++ index.js
@@ -1,52 +1,27 @@
-window.addEventListener("load", function () {
-    const sliderPrev = document.querySelector(".slider-prev");
-    const sliderNext = document.querySelector(".slider-next");
-    const slider = document.querySelector(".slider");
-    const sliderMain = document.querySelector(".slider-main");
-    const sliderItems = document.querySelectorAll(".slider-item");
-    const sliderDots = document.querySelectorAll(".slider-dot-item");
-    const sliderItemWidth = sliderItems[0].offsetWidth;
-    const sliderLength = sliderItems.length;
-    let positionX = 0;
-    let index = 0;
-    sliderNext.addEventListener("click", function () {
-        moveSlider(1);
-    });
-    sliderPrev.addEventListener("click", function () {
-        moveSlider(-1);
-    });
-    [...sliderDots].forEach((item) =>
-        item.addEventListener("click", function (e) {
-            [...sliderDots].forEach((el) => el.classList.remove("active"));
-            e.target.classList.add("active");
-            const slideIndex = parseInt(e.target.dataset.index);
-            index = slideIndex;
-            positionX = -1 * index * sliderItemWidth;
-            sliderMain.style = 'transform: translateX(' + positionX + 'px)';
-        })
-    )
-    function moveSlider(direction) {
-        if (direction === 1) {
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
 
-            if (index >= sliderLength - 1) {
-                index = sliderLength - 1;
-                return;
-            };
+let dot1 = document.getElementById("dot1");
+let dot2 = document.getElementById("dot2");
+let dot3 = document.getElementById("dot3");
 
-            positionX = positionX - sliderItemWidth;
-            sliderMain.style = 'transform: translateX(' + positionX + 'px)';
-            index++;
-        } else if (direction === -1) {
+dot1.onclick= image1;
+function image1(){
+    img1.style = "display = block"
+    img2.style = "display = none"
+    img3.style = "display = none"
+}
+dot2.onclick= image2;
+function image2(){
+    img1.style = "display = none"
+    img2.style = "display = block"
+    img3.style = "display = none"
+}
+dot3.onclick= image3;
+function image3(){
+    img1.style = "display = none"
+    img2.style = "display = none"
+    img3.style = "display = block"
+}
 
-            if (index <= 0) {
-                index = 0;
-                return;
-            };
-            positionX = positionX + sliderItemWidth;
-            sliderMain.style = 'transform: translateX(' + positionX + 'px)';
-            index--;
-        };
-        [...sliderDots].forEach((el) => el.classList.remove("active"));
-        sliderDots[index].classList.add("active");
-    }
-});
